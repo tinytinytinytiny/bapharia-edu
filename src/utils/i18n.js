ea8d8f5e5644e9343587a1b64faeddf5df730785
tinytinytinytiny <105854913+tinytinytinytiny@users.noreply.github.com>
2023-07-01 19:47:39 +0000

    move all images and data to storyblok

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,7 +1,44 @@
-import { ui, defaultLang } from '@data/i18n';
+import { useStoryblokApi } from '@storyblok/astro';
+import { languages, ui } from '@data/i18n';
 
-export function useTranslations(lang) {
-	return function t(key) {
-		return ui[lang][key] || ui[defaultLang][key] || key;
+const storyblokApi = useStoryblokApi();
+
+export async function useTranslations(lang) {
+	let strings;
+	const cachedStrings = ui.get();
+
+	if (cachedStrings) {
+		strings = cachedStrings;
+	} else {
+		strings = await getAllLangStrings();
+		ui.set(strings);
 	}
+
+	return function t(key) {
+		return strings[lang][key] || key;
+	}
+}
+
+async function getStrings(dimension) {
+	const { data: { datasource_entries }} = await storyblokApi.get('cdn/datasource_entries', {
+		datasource: 'ui',
+		dimension,
+		per_page: 1000
+	});
+
+	return Object.fromEntries(
+		datasource_entries.map(
+			x => ([x.name, x.dimension_value || x.value])
+		)
+	);
+}
+
+async function getAllLangStrings() {
+	return Object.fromEntries(
+		await Promise.all(
+			Object.keys(languages).map(async (lang) => 
+				([lang, await getStrings(lang)])
+		  	)
+		)
+	);
 }
