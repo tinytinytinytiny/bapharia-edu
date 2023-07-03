c4010e2a88dedbc991d5fc0209674d865bd089f8
tinytinytinytiny <tinynametaken@yahoo.com>
2023-07-03 13:46:07 -0400

    change icons

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -2,18 +2,18 @@ export default [
 	{
 		title: 'nav.home',
 		url: '/',
-		icon: import('@assets/icons/bapharia.svg?raw')
+		icon: import('@assets/icons/home.svg?raw')
 	},
 	{
 		title: 'nav.classes',
 		url: '/classes',
-		icon: import('@assets/icons/msq.svg?raw'),
+		icon: import('@assets/icons/double-sword.svg?raw'),
 		children: 'classes/'
 	},
 	{
 		title: 'nav.guides',
 		url: '/guides',
-		icon: import('@assets/icons/guide.svg?raw'),
+		icon: import('@assets/icons/info.svg?raw'),
 		children: 'guides/'
 	}
 ];
