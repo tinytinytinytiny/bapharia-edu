992bb2b3aa8de0eea85615d0732c2975dd17fbea
tinytinytinytiny <tinynametaken@yahoo.com>
2025-08-20 22:51:13 -0400

    make cards containers

--- src/data/design-tokens/font-sizes.cjs
+++ src/data/design-tokens/font-sizes.cjs
@@ -1,17 +1,17 @@
 const css = `
-/* @link https://utopia.fyi/type/calculator?c=328,16,1.125,1910,18,1.25,7,2,&s=0.75|0.5|0.25,1.5|2|3|4|6|8|12,s-l&g=xs,xs,4xl,12 */
+/* @link https://utopia.fyi/type/calculator?c=328,16,1.125,1724,18,1.25,7,2,&s=0.75|0.5|0.25,1.5|2|3|4|6|8|12,s-l&g=xs,xs,4xl,11 */
 
 :root {
-  --step--2: clamp(0.72rem, 0.8047rem + -0.0709cqi, 0.7901rem);
-  --step--1: clamp(0.8889rem, 0.8866rem + 0.0112cqi, 0.9rem);
-  --step-0: clamp(1rem, 0.9741rem + 0.1264cqi, 1.125rem);
-  --step-1: clamp(1.125rem, 1.0667rem + 0.2845cqi, 1.4063rem);
-  --step-2: clamp(1.2656rem, 1.1636rem + 0.4978cqi, 1.7578rem);
-  --step-3: clamp(1.4238rem, 1.2635rem + 0.7822cqi, 2.1973rem);
-  --step-4: clamp(1.6018rem, 1.3645rem + 1.1578cqi, 2.7466rem);
-  --step-5: clamp(1.802rem, 1.4638rem + 1.6498cqi, 3.4332rem);
-  --step-6: clamp(2.0273rem, 1.5578rem + 2.29cqi, 4.2915rem);
-  --step-7: clamp(2.2807rem, 1.6413rem + 3.1188cqi, 5.3644rem);
+  --step--2: clamp(0.72rem, 0.8066rem + -0.0804cqi, 0.7901rem);
+  --step--1: clamp(0.8889rem, 0.8863rem + 0.0127cqi, 0.9rem);
+  --step-0: clamp(1rem, 0.9706rem + 0.1433cqi, 1.125rem);
+  --step-1: clamp(1.125rem, 1.0589rem + 0.3223cqi, 1.4063rem);
+  --step-2: clamp(1.2656rem, 1.15rem + 0.5641cqi, 1.7578rem);
+  --step-3: clamp(1.4238rem, 1.2421rem + 0.8865cqi, 2.1973rem);
+  --step-4: clamp(1.6018rem, 1.3328rem + 1.3121cqi, 2.7466rem);
+  --step-5: clamp(1.802rem, 1.4188rem + 1.8696cqi, 3.4332rem);
+  --step-6: clamp(2.0273rem, 1.4953rem + 2.5951cqi, 4.2915rem);
+  --step-7: clamp(2.2807rem, 1.5562rem + 3.5344cqi, 5.3644rem);
 }
 `;
 
--- src/data/design-tokens/spacing.cjs
+++ src/data/design-tokens/spacing.cjs
@@ -1,33 +1,33 @@
 const css = `
-/* @link https://utopia.fyi/space/calculator?c=328,16,1.125,1910,18,1.25,7,2,&s=0.75|0.5|0.25,1.5|2|3|4|6|8|12,s-l&g=xs,xs,4xl,12 */
+/* @link https://utopia.fyi/space/calculator?c=328,16,1.125,1724,18,1.25,7,2,&s=0.75|0.5|0.25,1.5|2|3|4|6|8|12,s-l&g=xs,xs,4xl,11 */
 
 :root {
-  --space-3xs: clamp(0.25rem, 0.237rem + 0.0632vw, 0.3125rem);
-  --space-2xs: clamp(0.5rem, 0.487rem + 0.0632vw, 0.5625rem);
-  --space-xs: clamp(0.75rem, 0.7241rem + 0.1264vw, 0.875rem);
-  --space-s: clamp(1rem, 0.9741rem + 0.1264vw, 1.125rem);
-  --space-m: clamp(1.5rem, 1.4611rem + 0.1896vw, 1.6875rem);
-  --space-l: clamp(2rem, 1.9482rem + 0.2528vw, 2.25rem);
-  --space-xl: clamp(3rem, 2.9223rem + 0.3793vw, 3.375rem);
-  --space-2xl: clamp(4rem, 3.8963rem + 0.5057vw, 4.5rem);
-  --space-3xl: clamp(6rem, 5.8445rem + 0.7585vw, 6.75rem);
-  --space-4xl: clamp(8rem, 7.7927rem + 1.0114vw, 9rem);
-  --space-5xl: clamp(12rem, 11.689rem + 1.5171vw, 13.5rem);
+  --space-3xs: clamp(0.25rem, 0.2353rem + 0.0716vw, 0.3125rem);
+  --space-2xs: clamp(0.5rem, 0.4853rem + 0.0716vw, 0.5625rem);
+  --space-xs: clamp(0.75rem, 0.7206rem + 0.1433vw, 0.875rem);
+  --space-s: clamp(1rem, 0.9706rem + 0.1433vw, 1.125rem);
+  --space-m: clamp(1.5rem, 1.4559rem + 0.2149vw, 1.6875rem);
+  --space-l: clamp(2rem, 1.9413rem + 0.2865vw, 2.25rem);
+  --space-xl: clamp(3rem, 2.9119rem + 0.4298vw, 3.375rem);
+  --space-2xl: clamp(4rem, 3.8825rem + 0.5731vw, 4.5rem);
+  --space-3xl: clamp(6rem, 5.8238rem + 0.8596vw, 6.75rem);
+  --space-4xl: clamp(8rem, 7.765rem + 1.1461vw, 9rem);
+  --space-5xl: clamp(12rem, 11.6476rem + 1.7192vw, 13.5rem);
 
   /* One-up pairs */
-  --space-3xs-2xs: clamp(0.25rem, 0.1852rem + 0.3161vw, 0.5625rem);
-  --space-2xs-xs: clamp(0.5rem, 0.4223rem + 0.3793vw, 0.875rem);
-  --space-xs-s: clamp(0.75rem, 0.6723rem + 0.3793vw, 1.125rem);
-  --space-s-m: clamp(1rem, 0.8575rem + 0.6953vw, 1.6875rem);
-  --space-m-l: clamp(1.5rem, 1.3445rem + 0.7585vw, 2.25rem);
-  --space-l-xl: clamp(2rem, 1.7149rem + 1.3906vw, 3.375rem);
-  --space-xl-2xl: clamp(3rem, 2.689rem + 1.5171vw, 4.5rem);
-  --space-2xl-3xl: clamp(4rem, 3.4298rem + 2.7813vw, 6.75rem);
-  --space-3xl-4xl: clamp(6rem, 5.378rem + 3.0341vw, 9rem);
-  --space-4xl-5xl: clamp(8rem, 6.8597rem + 5.5626vw, 13.5rem);
+  --space-3xs-2xs: clamp(0.25rem, 0.1766rem + 0.3582vw, 0.5625rem);
+  --space-2xs-xs: clamp(0.5rem, 0.4119rem + 0.4298vw, 0.875rem);
+  --space-xs-s: clamp(0.75rem, 0.6619rem + 0.4298vw, 1.125rem);
+  --space-s-m: clamp(1rem, 0.8385rem + 0.788vw, 1.6875rem);
+  --space-m-l: clamp(1.5rem, 1.3238rem + 0.8596vw, 2.25rem);
+  --space-l-xl: clamp(2rem, 1.6769rem + 1.5759vw, 3.375rem);
+  --space-xl-2xl: clamp(3rem, 2.6476rem + 1.7192vw, 4.5rem);
+  --space-2xl-3xl: clamp(4rem, 3.3539rem + 3.1519vw, 6.75rem);
+  --space-3xl-4xl: clamp(6rem, 5.2951rem + 3.4384vw, 9rem);
+  --space-4xl-5xl: clamp(8rem, 6.7077rem + 6.3037vw, 13.5rem);
 
   /* Custom pairs */
-  --space-s-l: clamp(1rem, 0.7408rem + 1.2642vw, 2.25rem);
+  --space-s-l: clamp(1rem, 0.7063rem + 1.4327vw, 2.25rem);
 }
 `;
 
@@ -47,12 +47,18 @@ css.split('{')[1].split('}')[0].split(';')
     const [key, value] = x.split(': ');
     tokens[key] = remToPx(value);
     tokens[`${key}-rem`] = value;
+    tokens[`${key}-cqi-rem`] = vwToCqi(value);
   });
 
 function remToPx(string) {
   const matchRem = new RegExp(/[+-]?(\d*|\d{1,3}(,\d{3})*)(\.\d+)?(rem)/, 'g');
   const convertToPx = (x) => `${Math.round(Number(x.split('rem')[0]) * 16)}px`;
   return string.replaceAll(matchRem, convertToPx);
+}
+
+function vwToCqi(string) {
+  const matchVw = new RegExp(/(?<=\d)vw/, 'g');
+  return string.replaceAll(matchVw, 'cqi');
 }
 
 module.exports = tokens;  
