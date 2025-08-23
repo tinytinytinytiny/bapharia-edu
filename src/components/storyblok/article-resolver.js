b77414c2a4984cd36746c6dd383c89ea772d32dc
tinytinytinytiny <tinynametaken@yahoo.com>
2025-08-23 14:15:04 -0400

    move note icon to top right

--- src/components/storyblok/article-resolver.js
+++ src/components/storyblok/article-resolver.js
@@ -24,7 +24,7 @@ export default (component, blok) => {
 				caution:
 					'<path d="M12 9v5"/><path d="M12 17.5v.5"/><path d="M2.232 19.016L10.35 3.052c.713-1.403 2.59-1.403 3.302 0l8.117 15.964C22.45 20.36 21.544 22 20.116 22H3.883c-1.427 0-2.334-1.64-1.65-2.984z"/>',
 			};
-			return `<aside class="note" data-style="${blok.title.toLowerCase()}" aria-labelledby="${noteLabelId}">
+			return `<aside class="note" data-style="${blok.title.toLowerCase()}" aria-labelledby="${noteLabelId}" data-icon="top">
 				<div class="note-body stack">
 					<p class="h4" id="${noteLabelId}" aria-hidden="true">${blok.title}</p>
 					${renderRichText(blok.content)}
